d497c743dd901b7578288e9a48b174bab2ccb7e9
Najib Adan <najibadan1@gmail.com>
2025-05-18 05:21:21 +0300

    changed data layout

--- src/scrapper.py
+++ src/scrapper.py
@@ -1,3 +1,4 @@
+from random import randint
 import requests
 from bs4 import BeautifulSoup
 import json
@@ -10,9 +11,9 @@ base_url = "https://suumo.jp/jj/chintai/ichiran/FR301FC001/?ar=030&bs=040&page={
 output_dir = "data/apartments_json"
 os.makedirs(output_dir, exist_ok=True)
 
-# Define total pages (as retrieved previously, 4133)
+
 total_pages = 10_000
-t1 = time.now()
+
 for page in range(1, total_pages + 1):
     print(f"Scraping page {page}/{total_pages}")
     response = requests.get(base_url.format(page))
@@ -22,7 +23,7 @@ for page in range(1, total_pages + 1):
     cassette_items = soup.find_all("div", class_="cassetteitem")
 
     for item in cassette_items:
-        apartment_name = item.find(
+        property_name = item.find(
             "div", class_="cassetteitem_content-title"
         ).text.strip()
         location = item.find("li", class_="cassetteitem_detail-col1").text.strip()
@@ -34,7 +35,6 @@ for page in range(1, total_pages + 1):
             )
             if station.text.strip() != ""
         ]
-        stations_str = ", ".join(stations)
 
         rows = item.find_all("tr", class_="js-cassette_link")
         apartments = []
@@ -57,28 +57,29 @@ for page in range(1, total_pages + 1):
             )
             apartments.append(
                 {
-                    "Monthly Rent": rent,
-                    "Management Fee": admin_fee,
-                    "Shikikin (Deposit)": deposit,
-                    "Reikin (Gratuity)": gratuity,
-                    "Madori (Layout)": madori,
-                    "Menseki (Area)": menseki,
-                    "URL": apartment_url,
+                    "monthly_rent": rent,
+                    "management_fee": admin_fee,
+                    "deposit": deposit,  # deposit fee
+                    "gratuity": gratuity,
+                    "madori": madori,  # layout ie 1k, 2kdl
+                    "menseki": menseki,  # area of the apartment in m2
+                    "url": apartment_url,
                 }
             )
         apartment_data = {
-            "Apartment Name": apartment_name,
-            "Location": location,
-            "Stations": stations_str,
-            "Apartments": apartments,
+            "property_name": property_name,
+            "location": location,
+            "stations": stations,
+            "apartment": apartments,
         }
 
         # Save each apartment as a JSON file
-        file_name = f"{apartment_name}_{page}.json".replace("/", "_")
+        file_name = f"{property_name}_{page}.json".replace("/", "_")
         with open(
             os.path.join(output_dir, file_name), "w", encoding="utf-8"
         ) as json_file:
             json.dump(apartment_data, json_file, ensure_ascii=False, indent=4)
 
+    time.sleep(randint(1, 4))  # Randomly sleep between 1 - 4 seconds
 
 print(f"Data scraping complete! JSON files saved in {output_dir} directory")
