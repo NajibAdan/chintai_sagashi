0a494ec1c20039b8088fa9690749494f15ddad30
Najib Adan <najibadan1@gmail.com>
2026-08-19 06:44:38 +0900

    feat: added dbt models

---
+++ dbt/suumo/models/marts/fct_listing_snapshots.sql
@@ -0,0 +1,59 @@
+{{ config(
+    materialized='external',
+     location='s3://{{ env_var("BUCKET_NAME") }}/data/curated/{{ model.name }}.parquet'
+) }}
+with listings as (
+
+    select *
+    from {{ ref('stg_suumo_listings') }}
+
+),
+
+deduplicated as (
+
+    select *
+    from listings
+
+    qualify row_number() over (
+        partition by listing_id, crawl_date
+        order by source_file
+    ) = 1
+
+)
+
+select
+    listing_id,
+    crawl_date,
+
+    listing_id || '_' || cast(crawl_date as varchar)
+        as listing_snapshot_id,
+
+    prefecture,
+    city,
+
+    url,
+
+    property_name,
+    location,
+
+    rent_yen,
+    management_fee,
+
+    rent_yen + coalesce(management_fee, 0)
+        as monthly_cost_yen,
+    deposit,
+    gratuity,
+    floor_plan,
+    area_m2,
+
+    rent_yen / nullif(area_m2, 0)
+        as rent_per_m2,
+
+    floor,
+
+    building_age,
+    building_type,
+
+    stations
+
+from deduplicated
